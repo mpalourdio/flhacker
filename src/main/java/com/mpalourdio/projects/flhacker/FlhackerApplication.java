551028c17c70ed8bd0dd67c2eccdcf48040bd9cb
Michel Palourdio <mpalourdio@gmail.com>
2024-05-06 12:27:35 +0200

    chore: Split code. Fixe #4
    Former-commit-id: cd81f00652eca723a37e16ec38fd8e3cd46f7506

--- src/main/java/com/mpalourdio/projects/flhacker/FlhackerApplication.java
+++ src/main/java/com/mpalourdio/projects/flhacker/FlhackerApplication.java
@@ -9,98 +9,30 @@
 
 package com.mpalourdio.projects.flhacker;
 
-import io.github.seujorgenochurras.image.ascii.AsciiParser;
-import io.github.seujorgenochurras.image.ascii.ParserBuilder;
-import io.github.seujorgenochurras.image.ascii.algorithm.pixel.bright.Algorithms;
-import io.github.seujorgenochurras.image.ascii.algorithm.pixel.color.DefaultColorType;
-import org.apache.commons.cli.*;
-import org.apache.commons.io.FileUtils;
-import org.jaudiotagger.audio.AudioFileIO;
-import org.jaudiotagger.audio.exceptions.CannotReadException;
-import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
-import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
-import org.jaudiotagger.tag.TagException;
+import com.mpalourdio.projects.flhacker.utils.AudiofileHandler;
+import com.mpalourdio.projects.flhacker.utils.CliHandler;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 @SpringBootApplication
 public class FlhackerApplication {
 
-    static final String TMP_DIR = System.getProperty("java.io.tmpdir");
-    private static final String TMP_RESIZED_ARTWORK = TMP_DIR + "/resized.jpg";
-
-    private static final int TARGET_SIZE = 80;
-    public static final String FILE_CMD_LONG_OPTION = "file";
-
-    public static void main(String[] args) throws IOException, CannotReadException, TagException, InvalidAudioFrameException, ReadOnlyFileException {
+    public static void main(String[] args) {
         SpringApplication.run(FlhackerApplication.class, args);
     }
 
     @Bean
     CommandLineRunner run() {
         return args -> {
-            // init cleanup if files already exist
-            var tmpResizedFile = new File(TMP_RESIZED_ARTWORK);
-            FileUtils.deleteQuietly(tmpResizedFile);
-
-            // CLI handling
-            var options = new Options();
-            var input = new Option("f", FILE_CMD_LONG_OPTION, true, "input file path which containe the artwork to print");
-            input.setRequired(true);
-            options.addOption(input);
-
-            CommandLineParser parser = new DefaultParser();
-            var formatter = new HelpFormatter();
-            CommandLine cmd = null;
-
+            AudiofileHandler.setUp();
+            var cmd = CliHandler.run(args);
             try {
-                cmd = parser.parse(options, args);
-            } catch (ParseException e) {
-                System.out.println(e.getMessage());
-                formatter.printHelp("utility-name", options);
-                System.exit(1);
-            }
-
-            try {
-                // extract artwork
-                var audioFile = new File(cmd.getOptionValue(FILE_CMD_LONG_OPTION));
-                var extractedArtwork = AudioFileIO.read(audioFile)
-                        .getTag()
-                        .getFirstArtwork()
-                        .getImage();
-
-                // resize and save tmp artwork
-                var scaledArtwork = ((BufferedImage) extractedArtwork).getScaledInstance(TARGET_SIZE, TARGET_SIZE, Image.SCALE_DEFAULT);
-                var resized = new BufferedImage(TARGET_SIZE, TARGET_SIZE, BufferedImage.TYPE_INT_RGB);
-                resized.getGraphics().drawImage(scaledArtwork, 0, 0, null);
-
-                ImageIO.write(resized, "png", tmpResizedFile);
-
-                // tmp artwork to ascii art
-                var symbols = new String[]{" ", ".", "-", "I", "W", "@"};
-                var parserConfig = ParserBuilder.startBuild()
-                        .parserAlgorithm(Algorithms.HUMAN_EYE_ALGORITHM)
-                        .scaled()
-                        .height(TARGET_SIZE)
-                        .width(TARGET_SIZE)
-                        .getScale()
-                        .symbols(symbols)
-                        .colorAlgorithm(DefaultColorType.ANSI)
-                        .build();
-
-                var asciiArt = AsciiParser.parse(TMP_RESIZED_ARTWORK, parserConfig);
-
-                System.out.println(asciiArt);
+                AudiofileHandler.extractResizeSaveArtwork(cmd.getOptionValue(CliHandler.FILE_CMD_LONG_OPTION));
+                AudiofileHandler.generateAsciiArt();
             } finally {
-                FileUtils.deleteQuietly(tmpResizedFile);
+                AudiofileHandler.tearDown();
             }
         };
     }
