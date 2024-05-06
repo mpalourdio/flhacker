c79fa49bc450c07147b4edb9c3c5e1b90f39a4e0
Michel Palourdio <mpalourdio@gmail.com>
2024-05-06 12:28:31 +0200

    chore: update zip and add creation script
    Former-commit-id: 2a2451c72deaf382d459c0ea126b9d47ff35880b

---
+++ bin/genzip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -f bin/*.so
+rm -f bin/*.zip
+rm -f bin/flhacker
+
+cp target/*.so bin
+cp target/flhacker bin
+
+zip -j bin/flhacker.zip bin/*
+
+rm -f bin/*.so
+rm -f bin/flhacker
+
+git add bin/flhacker.zip
